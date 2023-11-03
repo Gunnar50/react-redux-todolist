eb359dfbb72ca702878f319f7841118f808496ad
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-11-03 10:30:29 +0000

    refractor

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { clearCompleted } from "../app/todo/todoSlice";
 
-function Sort() {
-	return <div>Sort</div>;
+function Sort({ setSort, activeTasks }) {
+	const dispatch = useDispatch();
+	const handleSort = (e) => {
+		const sortType = e.target.innerHTML.toLowerCase();
+		setSort(() => ({
+			all: false,
+			active: false,
+			completed: false,
+			[sortType]: true,
+		}));
+	};
+
+	return (
+		<div className="sort-buttons">
+			<button onClick={handleSort}>All</button>
+			<button onClick={handleSort}>Active</button>
+			<button onClick={handleSort}>Completed</button>
+		</div>
+	);
 }
 
 export default Sort;
