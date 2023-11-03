071642f41e1c79dc4ae94637a1e16acc9b6caea6
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-11-03 10:30:01 +0000

    refractor

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import React from "react";
-import { Counter } from "./components/Counter";
+import { useLocalStorage } from "@uidotdev/usehooks";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectTodos, setTasks } from "./app/todo/todoSlice";
 import Input from "./components/Input";
 import Sort from "./components/Sort";
+import TodoFooter from "./components/TodoFooter";
 import TodoList from "./components/TodoList";
 import "./style/App.scss";
 
 function App() {
+	const [sort, setSort] = useState({
+		all: true,
+		active: false,
+		completed: false,
+	});
+
+	const allTasks = useSelector(selectTodos);
+	if (!allTasks) return;
+	const activeTasks = allTasks.filter((task) => !task.completed);
+	const completedTasks = allTasks.filter((task) => task.completed);
+
+	const tasks = sort.all
+		? allTasks
+		: sort.active
+		? activeTasks
+		: completedTasks;
+
 	return (
-		<>
+		<div className="container">
+			<h1>TODO</h1>
 			<Input />
-			<TodoList />
-			<Sort />
-		</>
+			<div className="todo-container">
+				<TodoList tasks={tasks} />
+				<TodoFooter setSort={setSort} activeTasks={activeTasks} />
+			</div>
+		</div>
 	);
 }
 
