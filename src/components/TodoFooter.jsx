af077b553fed37df680002d6bbc08e659633e88f
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-11-04 15:21:38 +0000

    refractor

--- src/components/TodoFooter.jsx
+++ src/components/TodoFooter.jsx
@@ -1,21 +1,29 @@
+import { useWindowSize } from "@uidotdev/usehooks";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { clearCompleted } from "../app/todo/todoSlice";
 import "../style/TodoFooter.scss";
 import Sort from "./Sort";
 
-function TodoFooter({ setSort, activeTasks }) {
+function TodoFooter({ setSort, activeTasks, sort }) {
 	const dispatch = useDispatch();
+	const { width } = useWindowSize();
+	const isMobile = width < 530;
+
+	const sortComponent = () => (
+		<Sort setSort={setSort} activeTasks={activeTasks} sort={sort} />
+	);
 
 	return (
 		<div className="todo-footer">
-			<p>{activeTasks.length} items left</p>
-
-			<Sort setSort={setSort} activeTasks={activeTasks} />
-
-			<button onClick={() => dispatch(clearCompleted())}>
-				Clear Completed
-			</button>
+			<div className="footer">
+				<p>{activeTasks.length} items left</p>
+				{!isMobile ? sortComponent() : null}
+				<button onClick={() => dispatch(clearCompleted())}>
+					Clear Completed
+				</button>
+			</div>
+			{isMobile ? sortComponent() : null}
 		</div>
 	);
 }
